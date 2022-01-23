910c81fde9855c4e4b016a5157f16c44795bab0f
Dafdawer <59221438+Dafdawer@users.noreply.github.com>
2022-01-23 21:36:08 +0300

    finished but unpacked project

--- main.py
+++ main.py
@@ -1,52 +1,93 @@
-from datetime import datetime
+from operator import contains
 import requests
+import os
+from datetime import datetime
 from pathlib import Path
 from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+
+def save_file(file_, file_name):
+    try:
+        with open(file_name, 'wb') as file:
+            file.write(file_)
+            return 1
+    except FileNotFoundError:
+        print(f'unable to create {file_name} file')
+        return 0
+
 
 def get_picture(picture_url, file_name):
-    #Path(path_to_save).mkdir(parents=True, exist_ok=True)
-
-    response = requests.get(picture_url)
-    response.raise_for_status()
+    
+    try:
+        response = requests.get(picture_url)
+        response.raise_for_status()
+    except (
+        requests.exceptions.HTTPError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.MissingSchema
+    ):
+        print(f'couldn\'t open {picture_url} link')
+        return 0
 
     try:
         with open(file_name, 'wb') as file:
             file.write(response.content)
             return 1
     except FileNotFoundError:
+        print(f'unable to create {file_name} file')
         return 0
 
 def fetch_spacex_last_launch():
 
-    Path('./images/').mkdir(parents=True, exist_ok=True)
+    print('fetching SpaceX launch fotos...')
+
+    successfuly_fetched = 0
+    path_ = './images/spacex/'
+    Path(path_).mkdir(parents=True, exist_ok=True)
+
     url = 'https://api.spacexdata.com/v3/launches'
     payload = {"flight_number": 107}
-    #payload = {'launch_success': 'true', 'offset': 99}
     response = requests.get(url, params = payload)
     response.raise_for_status()
-    print(response.url)
-
     images = response.json()[0]['links']['flickr_images']
 
     for image_number, image in enumerate(images):
-        get_picture(image, f'./images/spacex{image_number}.jpg')
+        successfuly_fetched += get_picture(image, f'{path_}spacex{image_number}.jpg')
+
+    print(f'successfully downloaded {successfuly_fetched} out of {len(images)} images,')
+    print(f'destination folder: {path_}')
+    print('\n')
 
 
-def fetch_nasa_apod_last(api_key, save_path):
+def fetch_nasa_apod_last(api_key):
+    print('fetching NASA last APOD...')
 
+    save_path = f'./images/nasa/apod/last/{datetime.today().date()}/'
     Path(save_path).mkdir(parents=True, exist_ok=True)
 
     url = 'https://api.nasa.gov/planetary/apod'
-    response = requests.get(url, params={'api_key': api_key})
-    response.raise_for_status()
+    try:
+        response = requests.get(url, params={'api_key': api_key})
+        response.raise_for_status()
+    except (
+        requests.exceptions.HTTPError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.MissingSchema
+    ):
+        print(f'failed to get NASA last APOD')
+        return
 
     apod_url = response.json()['url']
     title = response.json()['title']
     extention = get_file_extention(apod_url)
-    file_name = f'{save_path}/{title}.{extention}'
+    file_name = f'{save_path}{title}.{extention}'
 
-    get_picture(apod_url, file_name)
-    print(apod_url)
+    if get_picture(apod_url, file_name) == 1:
+        print(f'successfully saved last APOD to {file_name}', '\n')
+        return
+    print('Fetching last NASA APOD was unsuccessful')
+    print('\n')
 
 
 def get_file_extention(url):
@@ -61,8 +102,18 @@ def get_apod_url_list(images_number, nasa_api_key):
         'count': images_number,
         'api_key': nasa_api_key
         }
-    response = requests.get(url, params=payload)
-    response.raise_for_status()
+
+    try:
+        response = requests.get(url, params=payload)
+        response.raise_for_status()
+    except (
+        requests.exceptions.HTTPError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.MissingSchema
+    ):
+        print(f'failed to get a set of random NASA APOD links')
+        return
+    
     links = response.json()
     
     for link in links:
@@ -73,43 +124,79 @@ def get_apod_url_list(images_number, nasa_api_key):
     return url_list
 
 def get_apod_images(url_list):
-    save_path = f'./images/{datetime.today().date()}'
-    Path(save_path).mkdir(parents=True, exist_ok=True)
-    successfully_downloaded = 0
+    print('Fetching a set of random NASA APOD images...')
 
+    save_path = f'./images/nasa/apod/random/{datetime.today().date()}/'
+    Path(save_path).mkdir(parents=True, exist_ok=True)
+
+    successfully_fetched = 0
     print(f'saving {len(url_list)} images to the {save_path} folder')
+    
     for title, url in url_list.items():
         extension_ = get_file_extention(url)
-        file_name = f'{save_path}/{title}.{extension_}'
-        successfully_downloaded += get_picture(url, file_name)
-    print(f'images downloaded: {successfully_downloaded}')
-    print(f"couldn't download images: {len(url_list) - successfully_downloaded}")
+        if ('embed' in url) or (len(extension_) > 4):
+            pass
+        file_name = f'{save_path}{title}.{extension_}'
+        successfully_fetched += get_picture(url, file_name)
+    
+    print(f'NASA random APOD images downloaded: {successfully_fetched}')
+    print(f"couldn't download: {len(url_list) - successfully_fetched} images", '\n')
+
+
+def get_nasa_earth_images(year, month, day, nasa_api):
+    print('Fetching NASA earth images...')
+
+    base_url = 'https://api.nasa.gov/EPIC/api/natural/date/'
+    target_url = f'{base_url}{year}-{month}-{day}'
+    payload = {'api_key': nasa_api}
+    response = requests.get(target_url, params = payload)
+    response.raise_for_status()
+    
+    image_list = []
+    successfully_fetched = 0
+
+    for image_meta in response.json():
+        image_list.append(image_meta['image'])
+
+    if not image_list:
+        print('Sorry, there are no images for the chosen date or wrong date format')
+        return 0
+
+    save_path = f'./images/nasa/earth/{year}-{month}-{day}/'
+    base_url = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month}/{day}/png/'
+    Path(save_path).mkdir(parents=True, exist_ok=True)
+
+    for file_name in image_list:
+        url = f'{base_url}{file_name}.png'
+        this_file_path = f'{save_path}{file_name}.png'
+
+        try:
+            response = requests.get(url, params=payload)
+            response.raise_for_status()
+            successfully_fetched += save_file(response.content, this_file_path)
+        except (
+        requests.exceptions.HTTPError,
+        requests.exceptions.ConnectionError,
+        requests.exceptions.MissingSchema
+        ):
+            print(f'unable to open {url} link')
+            pass
+
+    print(f'images downloaded: {successfully_fetched}')
+    print(f"couldn't download: {len(image_list) - successfully_fetched} images")
 
 
 def main():
-
-    #Path("./images").mkdir(parents=True, exist_ok=True)
-    # filename = './images/hubble.jpeg'
-    #path = './images/'
-    #url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    #get_picture(url, path)
-    #fetch_spacex_last_launch()
-    nasa_api_key = 'lkksr8w3HaiBV7H8PrWpLj3Se8WU0vR0tJhencOV'
-    #url = 'https://api.nasa.gov/planetary/apod'
-    #payload = {'count': 30, 'api_key': nasa_api_key}
-    #response = requests.get(url, params=payload)
-    #print(response.json())
-    #apod_links = get_apod_url_list(30, nasa_api_key)
-    #for title, url in apod_links.items():
-    #    print(title, url, sep=", ")
-    #print(datetime.today().date())
-    #fetch_nasa_apod(nasa_api_key)
-    url_list = get_apod_url_list(30, nasa_api_key)
-    get_apod_images(url_list)
+    load_dotenv()
+    nasa_api_key = os.getenv('NASA_API_KEY')
+    
+    fetch_spacex_last_launch()
+    fetch_nasa_apod_last(nasa_api_key)
+    get_apod_images(get_apod_url_list(30, nasa_api_key))
+    get_nasa_earth_images('2022', '01', '15', nasa_api_key)
 
 
 
 if __name__ == "__main__":
-    nasa_api_key = 'lkksr8w3HaiBV7H8PrWpLj3Se8WU0vR0tJhencOV'
     main()
  
