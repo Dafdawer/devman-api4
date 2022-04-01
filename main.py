62ea17ebc202ca37fc24e08742d3e696f877a34a
Dafdawer <59221438+Dafdawer@users.noreply.github.com>
2022-04-01 06:06:05 +0300

    0104

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from utilities import make_working_dir, do_tg_posting
 from time import sleep
 from dotenv import load_dotenv
 from random import shuffle
-from spacex import fetch_spacex_images, get_actual_spacex_media
-from nasa import fetch_nasa_images, get_earth_dates
+from utilities import download_pictures
+from spacex import get_actual_spacex_media
+from nasa import get_earth_dates, get_earth_links, return_apod_urls
 
 
 def main():
 
     load_dotenv()
+    nasa_api_key = os.getenv('NASA_API_KEY')
     telegram_token = os.getenv('TELEGRAM_BOT_TOKEN')
     tg_chat_id = os.getenv('TELEGRAM_CHAT_ID')
     delay_ = int(os.getenv('IMAGE_POSTING_DELAY_TIME'))
@@ -30,7 +32,8 @@ def main():
                 spacex_links = get_actual_spacex_media()
                 shuffle(spacex_links)
             if not earth_dates:
-                earth_dates = get_earth_dates()
+                earth_dates = get_earth_dates(nasa_api_key)
+                earth_dates.reverse()
         except HTTPError as e:
             logging.warning(e)
 
@@ -38,8 +41,17 @@ def main():
         working_dir = make_working_dir()
         print('Downloading images')
         try:
-            fetch_spacex_images(spacex_links, working_dir)
-            fetch_nasa_images(working_dir, earth_dates)
+            download_pictures(spacex_links[-3:], working_dir)
+            spacex_links = spacex_links[:-3]
+
+            earth_links = []
+            while not earth_links:
+                earth_links = get_earth_links(earth_dates.pop(0))
+            download_pictures(earth_links, working_dir)
+
+            apod_links = return_apod_urls(nasa_api_key, 3)
+            download_pictures(apod_links, working_dir)
+
         except HTTPError as e:
             logging.warning(e)
 
--- nasa.py
+++ nasa.py
@@ -1,24 +1,19 @@
 import requests
-from dotenv import load_dotenv
-from os import getenv
-from utilities import download_pictures
 
 
-load_dotenv()
-nasa_api_key = getenv('NASA_API_KEY')
-dates = []
-
-
-def get_earth_dates():
+def get_earth_dates(nasa_api_key):
+    dates = []
     url = 'https://api.nasa.gov/EPIC/api/natural/all'
     payload = {'api_key': nasa_api_key}
+
 
     response = requests.get(url, params=payload)
     response.raise_for_status()
     dates_raw = response.json()
 
-    for date in dates_raw:
-        dates.append(date['date'])
+    dates = [date['date'] for date in dates_raw]
+    # for date in dates_raw:
+    #     dates.append(date['date'])
 
     return dates
 
@@ -37,28 +32,24 @@ def convert_names_to_links(earth_image_names, date):
     return earth_image_urls
 
 
-def get_earth_links(dates):
+def get_earth_links(date):
 
     earth_links = []
 
-    for date in dates[-1::-1]:
-        url = f'https://epic.gsfc.nasa.gov/api/enhanced/date/{date}'
-        response = requests.get(url)
-        response.raise_for_status()
-        response_content = response.json()
+    url = f'https://epic.gsfc.nasa.gov/api/enhanced/date/{date}'
+    response = requests.get(url)
+    response.raise_for_status()
+    response_content = response.json()
 
-        for item in response_content:
-            earth_links.append(item['image'])   # only image names so far
+    for item in response_content:
+        earth_links.append(item['image'])   # only image names so far
 
-        if earth_links:         # could be empty list
-            earth_links = convert_names_to_links(earth_links, date)
-            dates.remove(date)  # remove used date
-            return earth_links[:3]  # 3 are enough
-
-        dates.remove(date)      # remove date w/o images
+    if earth_links:         # could be empty list
+        earth_links = convert_names_to_links(earth_links, date)
+        return earth_links[:3]  # 3 are enough
 
 
-def return_apod_urls(number=1):
+def return_apod_urls(nasa_api_key, number=1):
     urls = []
     url = 'https://api.nasa.gov/planetary/apod'
     payload = {
@@ -78,9 +69,3 @@ def return_apod_urls(number=1):
 
             if len(urls) >= number:
                 return urls[:number]
-
-
-def fetch_nasa_images(dirpath, dates):
-    links = get_earth_links(dates)
-    links.extend(return_apod_urls(3))
-    download_pictures(links, dirpath)
